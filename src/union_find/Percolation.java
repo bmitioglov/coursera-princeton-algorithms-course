4a3c52455bcba03f9aaf41bb485a0856f344a39d
Boris Mitioglov <bmitioglov@ooyala.com>
2018-10-01 19:39:58 +0300

    Implementation start for the percolation

--- src/union_find/Percolation.java
+++ src/union_find/Percolation.java
@@ -1,12 +1,40 @@
 package union_find;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+
+    private Integer n;
+    private Integer[] openclosedarray;
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+
     public Percolation(int n) {
+        this.n = n;
+        weightedQuickUnionUF = new WeightedQuickUnionUF(n*n);
 
+        openclosedarray = new Integer[n*n];
+
+        for (int i=0; i < n * n; i++) {
+            openclosedarray[i] = 0;
+        }
     }// create n-by-n grid, with all sites blocked
-    public    void open(int row, int col) {
 
+    public void open(int row, int col) {
+        Integer arrRow = row - 1;
+        Integer arrCol = col - 1;
+        if (!checkIndex(row) || !checkIndex(col)) {
+            throw new IllegalArgumentException("Wrong col, row index");
+        }
+        if (openclosedarray[(row-1)*n - col] != 1) {
+            openclosedarray[(row-1)*n - col] = 1;
+            //TODO: Union with the neighbors
+        }
     }   // open site (row, col) if it is not open already
+
+    private boolean checkIndex(int value) {
+        return value >= 1 && value <= n;
+    }
+
     public boolean isOpen(int row, int col) {
 
     }  // is site (row, col) open?
