02633e6ca610549d745b4360235154a0c41eb8bf
Boris Mitioglov <bmitioglov@ooyala.com>
2018-10-11 00:25:17 +0300

    3sum problem solution

--- src/union_find/ThreeSumNSquared.scala
+++ src/union_find/ThreeSumNSquared.scala
@@ -1,5 +1,7 @@
 package union_find
 
+import java.util
+
 import scala.util.Random
 
 class ThreeSumNSquared(n: Int) {
@@ -7,12 +9,49 @@ class ThreeSumNSquared(n: Int) {
   val array: Array[Int] = Array.ofDim[Int](n)
 
   for (i <- 0 until n) {
-    array(i) = Random.nextInt()
+    array(i) = Random.nextInt(n + 1 + n) - n
+  }
+
+  util.Arrays.sort(array)
+
+  def printObjectArray(): Unit = {
+    print ("Object Array:\t ")
+    for (i <- array.indices) {
+      print(array(i) + " ")
+    }
+    println()
+  }
+
+  val allPairs: List[String] = findPairs()
+
+  def findPairs(): List[String] = {
+    val aggList = List.newBuilder[String]
+    for (i <- 0 until array.length - 1) {
+      val key = array(i)
+      var start = i + 1
+      var end = n - 1
+      while (start < end) {
+        if (key + array(start) + array(end) == 0) {
+          aggList += (key + " " + array(start) + " " + array(end))
+          start += 1
+          end -= 1
+        } else if (key + array(start) + array(end) < 0) {
+          start += 1
+        } else if (key + array(start) + array(end) > 0) {
+          end -= 1
+        }
+      }
+    }
+    aggList.result()
   }
 
 
+}
+
+object ThreeSumNSquared {
   def main(args: Array[String]): Unit = {
-
+    val threeSumNSquared: ThreeSumNSquared = new ThreeSumNSquared(10)
+    threeSumNSquared.printObjectArray()
+    println(threeSumNSquared.allPairs)
   }
-
 }
