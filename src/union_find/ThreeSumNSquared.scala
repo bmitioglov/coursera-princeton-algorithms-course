6ecfff21cb1174a4be61c15d2ab39e916f2ee6ac
Boris Mitioglov <bmitioglov@ooyala.com>
2018-10-10 16:10:31 +0300

    template for 3sum alg added

---
+++ src/union_find/ThreeSumNSquared.scala
@@ -0,0 +1,18 @@
+package union_find
+
+import scala.util.Random
+
+class ThreeSumNSquared(n: Int) {
+
+  val array: Array[Int] = Array.ofDim[Int](n)
+
+  for (i <- 0 until n) {
+    array(i) = Random.nextInt()
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
