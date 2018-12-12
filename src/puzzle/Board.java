00c9baaa0965df3974cf62bb224d6a5ef9602c7d
Mitioglov Boris <ibmttz13.5@gmail.com>
2018-12-13 02:07:34 +0300

    Solver added

--- src/puzzle/Board.java
+++ src/puzzle/Board.java
@@ -3,6 +3,7 @@ package puzzle;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Board {
 
@@ -18,7 +19,7 @@ public class Board {
     private int[][] copyArray(int[][] old) {
         int[][] copy = new int[old.length][old.length];
         for(int i=0; i<old.length; i++)
-            for(int j=0; j<old[i].length; j++)
+            for(int j=0; j<old.length; j++)
                 copy[i][j] = old[i][j];
         return copy;
     }
@@ -48,21 +49,47 @@ public class Board {
         return distanceCounter + numberOfMoves;
     }                // sum of Manhattan distances between blocks and goal
     public boolean isGoal() {
-        return Arrays.equals(board, goalBoard);
+        return Arrays.deepEquals(board, goalBoard);
     }                // is this board the goal board?
 
     public Board twin() {
-        int[][] copyArray = Arrays.copyOf(board, board.length*board.length);
-        int temp = copyArray[0][0];
-        copyArray[0][0] = copyArray[0][1];
-        copyArray[0][1] = temp;
+        int[][] copyArray = copyArray(board);
+
+        int oneBasedRow = 0;
+        int oneBasedCol = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == 0) {
+                    oneBasedRow = i+1;
+                    oneBasedCol = j+1;
+                    break;
+                }
+            }
+        }
+
+        int candidate1Row = 0;
+        int candidate1Column = 0;
+        while (candidate1Row + 1 == oneBasedRow && candidate1Column + 1 == oneBasedCol) {
+            candidate1Row = new Random().nextInt(3);
+            candidate1Column = new Random().nextInt(3);
+        }
+
+        int candidate2Row = 0;
+        int candidate2Column = 0;
+        while (candidate2Row + 1 == oneBasedRow && candidate2Column + 1 == oneBasedCol) {
+            candidate2Row = new Random().nextInt(3);
+            candidate2Column = new Random().nextInt(3);
+        }
+
+
+        int temp = copyArray[candidate1Row][candidate1Column];
+        copyArray[candidate1Row][candidate1Column] = copyArray[candidate2Row][candidate2Column];
+        copyArray[candidate2Row][candidate2Column] = temp;
         return new Board(copyArray);
     }                   // a board that is obtained by exchanging any pair of blocks
-    public boolean equals(Object y) {
-        return Arrays.equals(board, (int[][]) y);
-    }        // does this board equal y?
 
-    public Iterable<Board> neighbors() {
+
+    public List<Board> neighbors() {
         int oneBasedRow = 0;
         int oneBasedCol = 0;
         List<Board> neighbors = new ArrayList<Board>();
@@ -199,10 +226,24 @@ public class Board {
         boardArg[row2-1][col2-1] = temp;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Board board1 = (Board) o;
+
+        return Arrays.deepEquals(board, board1.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
 
     public static void main(String[] args) {
         int[][] testBoard = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
         Board board = new Board(testBoard);
-        System.out.println(board.neighbors());
+        System.out.println(board.neighbors().size());
     } // unit tests (not graded)
 }
--- src/puzzle/Solver.java
+++ src/puzzle/Solver.java
@@ -1,20 +1,98 @@
 package puzzle;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.*;
+
 public class Solver {
+    List<Board> solution;
+    int minNumberOfModes = 0;
+    boolean isSolvable = true;
+
     public Solver(Board initial) {
+        MinPQ<Board> minPQ1 = new MinPQ<>(new Comparator<Board>() {
+            @Override
+            public int compare(Board o1, Board o2) {
+                if (o1.hamming() < o2.hamming()) {
+                    return -1;
+                } else if (o1.hamming() == o2.hamming()) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+        });
 
+        MinPQ<Board> minPQ2 = new MinPQ<>(new Comparator<Board>() {
+            @Override
+            public int compare(Board o1, Board o2) {
+                if (o1.hamming() < o2.hamming()) {
+                    return -1;
+                } else if (o1.hamming() == o2.hamming()) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+        });
+
+
+        List<Board> solution1 = new ArrayList<Board>();
+        List<Board> solution2 = new ArrayList<Board>();
+        List<Board> prevBoards1 = new ArrayList<>();
+        List<Board> prevBoards2 = new ArrayList<>();
+
+        Board current = initial;
+        Board currentTwin = initial.twin();
+        solution1.add(current);
+        solution2.add(currentTwin);
+
+        while (!current.isGoal() && !currentTwin.isGoal()) {
+            for (Board board: current.neighbors()) {
+                if (!prevBoards1.contains(board)) {
+                    minPQ1.insert(board);
+                    prevBoards1.add(board);
+                }
+            }
+            for (Board board: currentTwin.neighbors()) {
+                if (!prevBoards2.contains(board)) {
+                    minPQ2.insert(board);
+                    prevBoards2.add(board);
+                }
+            }
+            current = minPQ1.delMin();
+            currentTwin = minPQ2.delMin();
+            solution1.add(current);
+            solution2.add(currentTwin);
+            minNumberOfModes++;
+        }
+
+        if (current.isGoal()) {
+            solution = solution1;
+            isSolvable = true;
+        } else {
+            solution = solution2;
+            isSolvable = false;
+        }
     }          // find a solution to the initial board (using the A* algorithm)
+
     public boolean isSolvable() {
-
+        return isSolvable;
     }           // is the initial board solvable?
+
+
     public int moves() {
-
+        return minNumberOfModes;
     }                     // min number of moves to solve initial board; -1 if unsolvable
-    public Iterable<Board> solution() {
 
+    public Iterable<Board> solution() {
+        if (isSolvable()) {
+            return solution;
+        } else {
+            return Collections.emptyList();
+        }
     }      // sequence of boards in a shortest solution; null if unsolvable
 
     public static void main(String[] args) {
@@ -36,8 +114,12 @@ public class Solver {
             StdOut.println("No solution possible");
         else {
             StdOut.println("Minimum number of moves = " + solver.moves());
-            for (Board board : solver.solution())
+            int i = 0;
+            for (Board board : solver.solution()) {
+                System.out.println("step = " + i); //TODO: Remove before test
                 StdOut.println(board);
+                i++;
+            }
         }
     }
 }
