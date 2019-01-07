14dfab36d09c3a086dbe1f0abd38cf2535660c42
Tatiana Mitioglova <thaiwan7@gmail.com>
2019-01-07 16:25:19 +0300

    KdTrees insertion added

--- src/kdtree/KdTree.java
+++ src/kdtree/KdTree.java
@@ -4,18 +4,71 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
+
+    private enum Alignment {
+        Horizontal,
+        Vertical
+    }
+
+    private Node tree;
+    private int size;
+
+    private class Node {
+        private Alignment alignment;
+        private Point2D point;
+        private Node left;
+        private Node right;
+
+        private Node(Point2D point, Alignment alignment) {
+            this.point = point;
+            this.alignment = alignment;
+        }
+    }
+
     public KdTree() {
 
     }                              // construct an empty set of points
     public           boolean isEmpty()  {
-
+        return size == 0;
     }                    // is the set empty?
     public               int size()  {
-
+        return size;
     }                       // number of points in the set
-    public              void insert(Point2D p)  {
 
+    public void insert(Point2D p)  {
+        if (size == 0) {
+            tree = new Node(p, Alignment.Vertical);
+        } else {
+
+        }
     }            // add the point to the set (if it is not already in the set)
+
+    private void insertInKdTree(Node tree, Point2D point) {
+        if (tree.alignment == Alignment.Vertical) {
+            if (point.x() < tree.point.x()) {
+                if (tree.left == null) {
+                    tree.left = new Node(point, Alignment.Horizontal);
+                    size++;
+                 } else {
+                    insertInKdTree(tree.left, point);
+                }
+            } else {
+                insertInKdTree(tree.right, point);
+            }
+        } else {
+            if (point.y() < tree.point.y()) {
+                if (tree.right == null) {
+                    tree.right = new Node(point, Alignment.Vertical);
+                    size++;
+                } else {
+                    insertInKdTree(tree.left, point);
+                }
+            } else {
+                insertInKdTree(tree.right, point);
+            }
+        }
+    }
+
     public           boolean contains(Point2D p) {
 
     }         // does the set contain point p?
